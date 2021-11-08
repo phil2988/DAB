c6371fe82782f5fd0117a84b490634c4e6d03f3a
phil2988 <phillipblicher@hotmail.com>
2021-11-08 10:26:09 +0000

    P

---
+++ Assignments/DAB_Assignment_2_v2/DAB_Assignment_2_v2/Models/RoomProperties.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAB_Assignment_2_v2.Models
+{
+    class RoomProperties
+    {
+    }
+}
